53725cdc76a8320ff7b1985d3a13801b8b9fc857
Samy Mazazi <mazazi.samy@gmail.com>
2023-05-31 10:57:47 +0200

    second commit

---
+++ venv/test_pipeline_data.py
@@ -0,0 +1,13 @@
+from pipeline_data import compute_median_mean_diff
+import pytest
+
+def test_compute_median_mean_diff():
+    # Création de données de test
+    data = [ 4, 7]
+    df = pd.DataFrame({'Value': data})
+
+    # Calcul de la différence entre moyenne et médiane
+    result = compute_median_mean_diff(df)
+
+    # Vérification du résultat
+    assert result.loc['Value', 'Diff'] == 0.0
